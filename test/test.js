351987a4f151e50aa9cc577a4cb5e6cc8615cdef
ZerrickStaples <zerrick.staples@gmail.com>
2018-02-03 14:58:07 -0500

    Wrote failing test for returning 2 for one hour

--- test/test.js
+++ test/test.js
@@ -1,8 +1,17 @@
 import Mocha from 'mocha';
 import { expect } from 'chai';
+import { startTimeRate } from '../src/index';
 
 describe("Canary test", () => {
     it("True equals true", () => {
         expect(true).to.equal(true);
     })
+});
+
+describe("Babysitter kata", () => {
+    describe("Start time to bedtime", () => {
+        it("Return $12 for one hour", () => {
+            expect(startTimeRate()).to.equal(12);
+        });
+    });
 });
