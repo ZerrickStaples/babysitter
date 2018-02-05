d20f452f07d0adc75c7044b48b7bd5cf5f0efb09
ZerrickStaples <zerrick.staples@gmail.com>
2018-02-05 14:54:18 -0500

    Wrote failing test for restricting end time to 4 AM

--- test/test.js
+++ test/test.js
@@ -19,24 +19,32 @@ describe("Babysitter kata", () => {
             expect(startTimeRate(5, 8)).to.equal(36);
         });
 
-        it("Restrict start time to 5pm", () => {
+        it("Restrict start time to 5 PM", () => {
             expect(startTimeRate(4, 6)).to.equal(12);
         });
     });
     describe("Bedtime to midnight", () => {
+
         it("Return $8 for one hour", () => {
             expect(bedtimeRate(11, 12)).to.equal(8);
         });
+
         it("Return $8 per hour for multiple hours", () => {
             expect(bedtimeRate(9, 12)).to.equal(24);
         });
     });
     describe("Midnight to end", () => {
+
         it("Return $16 for one hour", () => {
             expect(endTimeRate(12, 1)).to.equal(16);
         });
+
         it("Return $16 per hour for multiple hours", () => {
             expect(endTimeRate(12, 3)).to.equal(48);
+        });
+
+        it("Restrict end time to 4 AM", () => {
+            expect(endTimeRate(12, 5)).to.equal(64);
         });
     });
 });
